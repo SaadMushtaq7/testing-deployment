fec45de3e87b99de45a9f7e9a8937f5bfe2e4133
SaadMushtaq7 <saad.mushtaq@emumba.com>
2023-01-05 10:28:53 +0500

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/api", (req, res) => {
+  res.send("This is to test if all the paths are deployed correctly");
+});
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
